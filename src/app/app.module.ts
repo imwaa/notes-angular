0a21a0bfd3e684e6588456e29df6a3f82baff1a7
imwaa <walid.dev@outlook.fr>
2021-02-18 16:47:26 +0100

    Added search bar & card component and styled them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesListComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
